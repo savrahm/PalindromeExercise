7d7e977fc389d140c351dad193ca88d00aaf76c0
Sarah <sarah.b.avram@gmail.com>
2020-09-14 18:29:37 -0400

    palindrome checker and test working

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public bool IsAPalindrome(string str)
+        {
+            int start = 0;
+            int end = str.Count()-1;
+            bool isPalindrome = true;
+
+            for (start = 0; start < end; start++, end--)
+            {
+                if (str[start] == str[end])
+                    isPalindrome = true;
+                else
+                    isPalindrome = false;
+            }
+
+            return isPalindrome;
+        }
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,13 +1,26 @@
+using PalindromeExercise;
 using System;
+using System.ComponentModel;
 using Xunit;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar",true)]
+        [InlineData("neveroddoreven",true)]
+        [InlineData("hello",false)]
+        public void Test1(string str, bool expected)
         {
+            //Arrange
+            var test = new WordSmith();
+
+            //Act
+            var actual = test.IsAPalindrome(str);
+
+            //Assert
+            Assert.Equal(actual, expected);
 
         }
     }
